fb40bab546cfc022c27fb36267945c3aef892f66
김승현 <kimsh0324@hanmail.net>
2024-10-28 23:33:57 +0900

    ch5_Rendering Elements

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,12 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
+
+/* chaper 3 */
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Library />
   </React.StrictMode>
 );
+*/
+
+/* chaper 4 */ 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>
+    //, document.getElementById('root')
+  );
+}, 1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
